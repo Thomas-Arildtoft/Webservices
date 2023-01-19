2aae437ca267511700c8e9dea225a3dffd7f68c2
gingerjx <piotrkalota99@gmail.com>
2023-01-19 11:44:09 +0200

    .

--- end-to-end-tests/deploy.sh
+++ end-to-end-tests/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 docker image prune -f
-docker ps -aq | xargs docker stop | xargs docker rm
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
 docker-compose up -d rabbitMq
 sleep 10
 docker-compose up -d account-management token-management payment-management customer merchant
